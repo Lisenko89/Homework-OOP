7891463dad3a3de51ac4b39f30e284be52dcdaea
Алексей Лисенко <lisenko-av@bk.ru>
2022-08-07 17:57:36 +0300

    Задание по ООП

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.birthday.day = 5;
+        post.birthday.month = 12;
+        post.birthday.year = 1950;
+        post.name = "Иван";
+        post.serialPassport = 4510;
+        post.numberPassport = 185746;
+        post.patronymic = "Иванович";
+        post.phone = "+79995552211";
+        post.surname = "Иванов";
+        post.subscription = true;
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+        public String name; // поле для имени
+        public int serialPassport;
+        public int numberPassport;
+        public String patronymic;
+        public String phone;
+        public String surname;
+        boolean subscription;
+        FormDate birthday = new FormDate();
+}
